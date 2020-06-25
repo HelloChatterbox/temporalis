6f13a95f117c5c814d01630c391da874069dc867
jarbasal <jarbasai@mailfence.com>
2020-06-25 01:55:51 +0100

    weatherbit

--- pyweather/providers/weatherbit.py
+++ pyweather/providers/weatherbit.py
@@ -1,235 +1,213 @@
+from pyweather.exceptions import InvalidKey
 from pyweather.location import geolocate
-import requests
 from pyweather.providers import WeatherProvider
-from pprint import pprint
-import pendulum
+from pyweather import WeatherData, DataPoint
 
 
 class WeatherBit(WeatherProvider):
-    def __init__(self, lat, lon, key, date=None, units="metric"):
+    default_key = "fd088ec43bef4769b87d347d3dec83e5"
+
+    def __init__(self, lat, lon, key=None, date=None, units="metric"):
         if units in ["english", "imperial", "us"]:
             units = "I"
-        elif units != "metric":
+        elif units.startswith("m"):
             units = "M"
         else:
             units = "S"
         super().__init__(lat, lon, date, units)
-        self.key = key
+        self.key = key or self.default_key
         self.url = "https://api.weatherbit.io/v2.0/current"
         self._request()
 
     @staticmethod
-    def from_address(address, key):
+    def from_address(address, key=None):
         lat, lon = geolocate(address)
         return WeatherBit(lat, lon, key)
 
     def _request_current(self):
         params = {"units": self.units,
                   "lat": self.latitude,
-                  "lon":  self.longitude,
-                  "key":self.key}
-        entry = requests.get(self.url, params=params).json()
+                  "lon": self.longitude,
+                  "key": self.key}
+        entry = self.session.get(self.url, params=params).json()
         entry = entry["data"][0]
-
-        # TODO hour to timestamp
-        sunriseTime = entry["sunrise"]
-        sunsetTime = entry["sunset"]
-
+        # {'rh': 84, 'pod': 'n', 'lon': -9.14, 'pres': 1003,
+        # 'timezone': 'Europe/Lisbon', 'ob_time': '2020-06-25 00:20',
+        # 'country_code': 'PT', 'clouds': 68, 'ts': 1593044400, 'solar_rad': 0,
+        # 'state_code': '14', 'city_name': 'Lisbon', 'wind_spd': 1.79,
+        # 'last_ob_time': '2020-06-25T00:20:00', 'wind_cdir_full': 'northwest',
+        # 'wind_cdir': 'NW', 'slp': 1015.5, 'vis': 5, 'h_angle': -90,
+        # 'sunset': '20:05', 'dni': 0, 'dewpt': 14.5, 'snow': 0, 'uv': 0,
+        # 'precip': 0, 'wind_dir': 326, 'sunrise': '05:12', 'ghi': 0, 'dhi': 0,
+        # 'aqi': 45, 'lat': 38.72, 'weather': {'icon': 'c02n', 'code': '802',
+        # 'description': 'Scattered clouds'}, 'datetime': '2020-06-25:00',
+        # 'temp': 17.2, 'station': 'C3643', 'elev_angle': -27.23,
+        # 'app_temp': 17.3}
+        """
+    TODO 
+    
+    slp: Sea level pressure (mb).
+    wind_cdir: Abbreviated wind direction.
+    wind_cdir_full: Verbal wind direction.
+    dewpt: Dew point (default Celcius).
+    aqi: Air Quality Index [US - EPA standard 0 - +500]
+    dhi: Diffuse horizontal solar irradiance (W/m^2) [Clear Sky]
+    dni: Direct normal solar irradiance (W/m^2) [Clear Sky]
+    ghi: Global horizontal solar irradiance (W/m^2) [Clear Sky]
+    solar_rad: Estimated Solar Radiation (W/m^2).
+    elev_angle: Solar elevation angle (degrees).
+    h_angle: Solar hour angle (degrees).
+        """
         pressure = entry.get("pres")
-        cloudCover = entry.get("clouds")
-        visibility = entry.get("vis")
+        if pressure is not None:
+            pressure = DataPoint("Pressure", pressure, "mb")
 
-        # TODO check docs for key
-        humidity = entry.get("humidity")
+        cloudCover = entry.get("clouds")
+        if cloudCover is not None:
+            cloudCover = DataPoint("CloudCover", cloudCover, "%")
+
+        visibility = entry.get("vis")
+        if visibility is not None:
+            if self.units == "M" or self.units == "S":
+                visibility = DataPoint("Visibility", visibility, "km")
+            elif self.units == "I":
+                # TODO  unit
+                visibility = DataPoint("Visibility", visibility, "km")
+
+        precip = entry.get("precip")
+        if precip is not None:
+            if self.units == "M" or self.units == "S":
+                precip = DataPoint("PrecipitationIntensity", precip, "mm/hr")
+            elif self.units == "I":
+                # TODO unit
+                precip = DataPoint("PrecipitationIntensity", precip, "mm/hr")
+
+        snow = entry.get("snow")
+        if snow is not None:
+            if self.units == "M" or self.units == "S":
+                snow = DataPoint("SnowIntensity", snow, "mm/hr")
+            elif self.units == "I":
+                # TODO  unit
+                snow = DataPoint("SnowIntensity", snow, "mm/hr")
+
+        humidity = entry.get("rh")
+        if humidity is not None:
+            humidity = DataPoint("Humidity", humidity, "%")
+
+        uv = entry.get("uv")
+        if uv is not None:
+            uv = DataPoint("UV Index", uv, "dimensionless")
 
         temperature = entry.get("temp")
-        ap_temperature = entry.get("app_temp") or temperature
-        temperature_min = entry.get("temp_min") or temperature
-        temperature_max = entry.get("temp_max") or temperature
+        if temperature is not None:
+            if self.units == "M":
+                temperature = DataPoint("Temperature", temperature, "ºC")
+            elif self.units == "S":
+                temperature = DataPoint("Temperature", temperature, "K")
+            elif self.units == "I":
+                temperature = DataPoint("Temperature", temperature, "ºF")
+
+        ap_temperature = entry.get("app_temp")
+        if ap_temperature is not None:
+            if self.units == "M":
+                ap_temperature = DataPoint("ApparentTemperature",
+                                           ap_temperature, "ºC")
+            elif self.units == "S":
+                ap_temperature = DataPoint("ApparentTemperature",
+                                           ap_temperature, "K")
+            elif self.units == "I":
+                ap_temperature = DataPoint("ApparentTemperature",
+                                           ap_temperature, "ºF")
+
+        dew = entry.get("dewpt")
+        if dew is not None:
+            if self.units == "M":
+                dew = DataPoint("DewPoint", dew, "ºC")
+            elif self.units == "S":
+                dew = DataPoint("DewPoint", dew, "K")
+            elif self.units == "I":
+                dew = DataPoint("DewPoint", dew, "ºF")
 
         wind_speed = entry.get("wind_spd")
+        if wind_speed is not None:
+            if self.units == "M" or self.units == "S":
+                wind_speed = DataPoint("WindSpeed", wind_speed, "m/s")
+            elif self.units == "I":
+                wind_speed = DataPoint("WindSpeed", wind_speed, "mph")
+
         wind_bearing = entry.get("wind_dir")
-        _w = entry.get("weather", [])
+        if wind_bearing is not None:
+            wind_bearing = DataPoint("WindBearing", wind_bearing, "º")
+
+        _w = entry.get("weather", {})
         icon = ""
         summary = ""
         if _w:
-            icon = _w.get("code", "") # TODO icon code to str
-            code_to_ico = {
-
-            }
+            icon = _w.get("code", "").lower()
             summary = _w.get("description") or icon
 
-        uvIndex = entry.get("uv")
-        solar_rad = entry.get("solar_rad")
-        snow = entry.get("snow")
-        sea_pressure = entry.get("slp")
-        humidity = entry.get("rh")
-
-        dewPoint = entry.get("dewpt")
-
-        precip = entry.get("precip")
-
-        timezone = entry["timezone"]
-
-        lat = entry.get("coord", {}).get("lat") or self.latitude
-        lon = entry.get("coord", {}).get("lat") or self.longitude
-
-        date = pendulum.parse(entry["ob_time"], tz=timezone)
+        ts = entry["ts"]
+        date = self._stamp_to_datetime(ts)
         w = {
-            "time": date.timestamp(),
-            "timezone": timezone,
+            "datetime": date,
             "pressure": pressure,
             "cloudCover": cloudCover,
             "visibility": visibility,
             "humidity": humidity,
             "temperature": temperature,
             "apparentTemperature": ap_temperature,
-            "temperatureMin": temperature_min,
-            "temperatureMax": temperature_max,
             "windSpeed": wind_speed,
             "windBearing": wind_bearing,
-            "dewPoint": dewPoint,
-            "uvIndex": uvIndex,
-            "sunriseTime": sunriseTime,
-            "sunsetTime": sunsetTime,
             "summary": summary,
             "icon": icon,
-            "precipIntensity": precip
+            "precipitation": precip,
+            "uvIndex": uv,
+            "snow": snow,
+            "dewPoint": dew
         }
 
-        hour = w
-        hour["time"] = date.replace(minute=0, microsecond=0, second=0).timestamp()
-        self.data["timezone"] = timezone
-        self.data["latitude"] = lat
-        self.data["longitude"] = lon
+        hour = WeatherData.from_dict(w)
+
+        self.data["timezone"] = self.timezone
+        self.data["latitude"] = self.latitude
+        self.data["longitude"] = self.longitude
         self.data["currently"] = w
         self.data["daily"] = {"summary": w["summary"],
                               "icon": w["icon"],
-                              "data": [w]}
+                              "data": [hour]}
         self.data["hourly"] = {"summary": w["summary"],
                                "icon": w["icon"],
                                "data": [hour]}
 
     def _request_forecast(self):
-        if self.units == "auto":
-            ACUWEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?lat" \
-                      "={lat}&lon={lon}&appid={key}"
-            url = ACUWEATHER_URL.format(key=self.key, lat=self.latitude, lon=self.longitude)
-        else:
-            ACUWEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?lat" \
-                      "={lat}&lon={lon}&appid={key}&units={units}"
-            url = ACUWEATHER_URL.format(key=self.key, lat=self.latitude, lon=self.longitude,
-                                 units=self.units)
+        params = {"units": self.units,
+                  "lat": self.latitude,
+                  "lon": self.longitude,
+                  "key": self.key}
+        res = self.session.get(
+            "https://api.openweathermap.org/data/2.5/forecast",
+            params=params).json()
 
-        res = requests.get(url).json()
+        if res["cod"] == 401:
+            raise InvalidKey(res["message"])
 
-        lat = res.get("city", {}).get("coord", {}).get("lat") or self.latitude
-        lon = res.get("city", {}).get("coord", {}).get("lon") or self.longitude
-        sunriseTime = res.get("city", {}).get("sunrise")
-        sunsetTime = res.get("city", {}).get("sunset")
-        # TODO shift in seconds from UTC
-        # mutate date object + update timezone
-        # timezone = res.get("city", {}).get("timezone")
-        timezone = "UTC"
-
-        # ACUWEATHER returns 3h in 3h readings
-        hours = []
-        _days = {}
-        for entry in res["list"]:
-            pressure = entry["main"].get("pressure")
-            cloudCover = entry.get("clouds", {}).get("all")
-            if cloudCover:
-                cloudCover = cloudCover
-            visibility = entry.get("visibility")
-            if visibility:
-                # meter -> km
-                visibility = visibility / 1000
-            humidity = entry["main"].get("humidity")
-            if humidity:
-                humidity = humidity
-            temperature = entry["main"].get("temp")
-            ap_temperature = entry["main"].get("feels_like") or temperature
-            temperature_min = entry["main"].get("temp_min") or temperature
-            temperature_max = entry["main"].get("temp_max") or temperature
-            wind_speed = entry.get("wind", {}).get("speed")
-            wind_bearing = entry.get("wind", {}).get("deg")
-            _w = entry.get("weather", [])
-            icon = ""
-            summary = ""
-            if _w:
-                icon = _w[0].get("main", "").lower()
-                summary = _w[0].get("description") or icon
-
-            rain1h = entry.get("rain", {}).get("1h")
-            rain3h = entry.get("rain", {}).get("3h")
-            if rain3h and not rain1h:
-                rain1h = rain3h / 3
-
-            pprint(entry)
-            w = {
-                "time": entry["dt"],
-
-                "timezone": timezone,
-
-                "pressure": pressure,
-                "cloudCover": cloudCover,
-                "visibility": visibility,
-                "humidity": humidity,
-                "temperature": temperature,
-                "apparentTemperature": ap_temperature,
-                "temperatureMin": temperature_min,
-                "temperatureMax": temperature_max,
-                "windSpeed": wind_speed,
-                "windBearing": wind_bearing,
-                "summary": summary,
-                "icon": icon,
-                "precipIntensity": rain1h or rain3h
-            }
-            hours.append(w)
-            w = WeatherDataPoint().from_dict(w)
-
-            if w.datetime.day not in _days:
-                _days[w.datetime.day] = []
-            _days[w.datetime.day] += [w]
-
-        # TODO
-        hourly_summary, hourly_icon = self._calc_hourly_summary(hours)
-        self.data["timezone"] = timezone
-        self.data["latitude"] = lat
-        self.data["longitude"] = lon
-
-        self.data["hourly"] = {"summary": hourly_summary,
-                               "icon": hourly_icon,
-                               "data": hours}
-        days = []
-        for d in _days:
-            day = self._calc_day_average(_days[d])
-            day = day.as_dict()
-
-            day["sunriseTime"] = sunriseTime
-            day["sunsetTime"] = sunsetTime
-            days += [day]
-
-        daily_summary, daily_icon = self._calc_daily_summary(days)
-        self.data["daily"] = {"summary": daily_summary,
-                              "icon": daily_icon,
-                              "data": days}
+        raise NotImplementedError
 
     def _request(self):
         self._request_current()
-        #self._request_forecast()
+        # TODO - need paid key
+        # self._request_forecast()
 
 
 if __name__ == "__main__":
-    KEY = "fd088ec43bef4769b87d347d3dec83e5"
     lat, lon = 38.7222563442538, -9.1393314973889
-    d = WeatherBit(lat, lon, KEY)
+    d = WeatherBit(lat, lon)
 
     print("##### CURRENT WEATHER ######")
     d.print()
 
     ## WIP ZONE
-    exit()
+
     print("\n##### DAILY FORECAST ######")
     d.print_daily()
 
